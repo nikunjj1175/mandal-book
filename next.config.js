92e4c5c3c7bb5fcf31d8f3737588a5d4f39afcde
1175🤍 <nikunjkorat117@gmail.com>
2025-12-01 18:14:15 +0530

    next config change

--- next.config.js
+++ next.config.js
@@ -4,12 +4,25 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
+  experimental: {
+    // Ensure Tesseract.js core/worker/WASM files are bundled for serverless functions
+    outputFileTracingIncludes: {
+      '/pages/api/contribution/upload-slip': [
+        './node_modules/tesseract.js-core/**/*',
+        './node_modules/tesseract.js/dist/worker.min.js',
+      ],
+      '/pages/api/loan/pay': [
+        './node_modules/tesseract.js-core/**/*',
+        './node_modules/tesseract.js/dist/worker.min.js',
+      ],
+      '/pages/api/user/upload-documents': [
+        './node_modules/tesseract.js-core/**/*',
+        './node_modules/tesseract.js/dist/worker.min.js',
+      ],
     },
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
 
